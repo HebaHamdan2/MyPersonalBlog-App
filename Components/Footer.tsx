c0d408c73a3e731fe77688efcebb9547848beb6a
HebaHamdan2 <hebasaeedhamdan296@gmail.com>
2024-10-22 14:10:37 +0300

    Blog Lsit design with static data ,filter blog by category ,start footer

---
+++ Components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center '>
+    <p className='text-sm text-white'>&copy; 2024 Heba Hamdan. All rights reserved.</p>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+'use client'
+import { BlogLsit } from "@/Components/BlogLsit";
+import { Footer } from "@/Components/Footer";
 import { Header } from "@/Components/Header";
+
 
 export default function Home() {
   return (
  <>
 <Header/>
+<BlogLsit/>
+<Footer/>
  </>
   );
 }
