2491a0c72fbe41c5ae1d4f37c4c68e94fa70dc09
HebaHamdan2 <hebasaeedhamdan296@gmail.com>
2024-10-22 18:32:15 +0300

    create blog post page based on id params, blog app done with static data

--- Components/BlogItem.tsx
+++ Components/BlogItem.tsx
@@ -1,15 +1,19 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-export const BlogItem = ({title,image,description,category}) => {
+export const BlogItem = ({title,image,description,category,id}) => {
   return (
     <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
-     <Image src={image} alt='image-blog' width={400} height={400} className='border-b border-black '  />
-    <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category} </p>
+    <Link href={`/blogs/${id}`}>
+    <Image src={image} alt='image-blog' width={400} height={400} className='border-b border-black '  />
+
+    </Link>
+       <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category} </p>
   <div className='p-5'>
     <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{title}</h5>
   <p className='mb-3 text-sm tracking-tight text-gray-700'>{description}</p>
- <div className='inline-flex items-center py-2 font-semibold text-center '>Read more</div>
+ <Link href={`/blogs/${id}`} className='inline-flex items-center py-2 font-semibold text-center '>Read more</Link>
   </div>
     </div>
   )
---
+++ app/blogs/[id]/page.tsx
@@ -0,0 +1,75 @@
+"use client"; 
+
+import { Footer } from '@/Components/Footer';
+import { blog_data } from '@/static/staticdata';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+
+type Props = {
+    params: Promise<{
+        id?: string;
+    }>;
+}
+
+const Page = ({ params }: Props) => {
+    const [data, setData] = useState(null);
+    const [id, setId] = useState<string | undefined>(undefined);
+
+    useEffect(() => {
+        const fetchParams = async () => {
+            const resolvedParams = await params;
+            setId(resolvedParams.id);
+        };
+        fetchParams();
+    }, [params]);
+
+    const fetchBlogData = async () => {
+        if (id) {
+            const blogItem = blog_data.find(blog => Number(id) === blog.id);
+            if (blogItem) {
+                setData(blogItem);
+            }
+        }
+    };
+
+    useEffect(() => {
+        fetchBlogData();
+    }, [id]);
+    
+  return (data?<>
+    <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
+    <div className='flex justify-between items-center'>
+        <Link href='/'>
+        <Image src='/bloglogo-bg.png' width={180} height={100} alt='' className='w-[130px] sm:w-quto' priority />
+        </Link>
+          <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>Get started</button>
+    </div>
+    <div className='text-center my-24'>
+        <h1 className='text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto'>{data.title}</h1>
+        <Image className='mx-auto mt-6 border border-white rounded-full' src={data.author_img} width={60} height={60} alt='Heba Hamdan' />
+        <p className='mt-1 pb-2 text-lg max-w-[740px] mx-auto '>{data.author}</p>
+    </div>  
+    </div>
+    <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10'>
+        <Image className='border-4 border-white' src={data.image} width={1280} height={720} alt='blog pic' />
+        <h1 className='my-8 text-[26px] font-semibold'>Introduction</h1>
+ <p>{data.description}</p>
+ <div className='my-24' >
+        <p>Share this article on social media </p>
+        <div className="flex">
+            <Image src='/facebook.svg' width={50} height={50} alt='facebook' />
+            <Image src='/instagram.svg' width={50} height={50} alt='instagram' />
+            <Image src='/linkedin.svg' width={50} height={50} alt='LinkedIn' />
+        </div>
+     </div>
+ <div > 
+ </div>
+     </div>
+    <Footer/>
+  </>
+  :<></>
+);
+};
+
+export default Page;
