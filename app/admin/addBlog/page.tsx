f97eee57f6ebf20620d93123cfa23ca9b8f42cc2
HebaHamdan2 <hebasaeedhamdan296@gmail.com>
2024-10-23 16:29:25 +0300

    add Blog functions from admin panel

--- app/admin/addBlog/page.tsx
+++ app/admin/addBlog/page.tsx
@@ -1,14 +1,69 @@
 "use client"
-import React from 'react'
+import axios from 'axios';
+import Image from 'next/image'
+import { title } from 'process';
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
 
 const page = () => {
+  const [image,setImage]=useState(null);
+  const [data,setData]=useState({
+    title:"",
+    description:"",
+    category:"Startup",
+    author:"Heba Hamdan",
+    authorImage:"/profile_icon.png"
+  });
+  const onChangeHandler=(event)=>{
+    const name=event.target.name;
+    const value=event.target.value;
+    setData(data=>({...data,[name]:value}));
+    console.log(data)
+  }
+  const onSubmitHandler=async(e)=>{
+  e.preventDefault();
+  const formData=new FormData();
+  formData.append('title',data.title);
+  formData.append('description',data.description);
+  formData.append('category',data.category);
+  formData.append('author',data.author);
+  formData.append('authorImage',data.authorImage);
+  formData.append('image',image);
+  const response=await axios.post('/api/blogs',formData);
+  if(response.data.success){
+    toast.success(response.data.msg);
+    setImage(null);
+    setData({
+      title:"",
+      description:"",
+      category:"Startup",
+      author:"Heba Hamdan",
+      authorImage:"/profile_icon.png"
+    })
+  }else{
+    toast.error("Error");
+  }
+  }
   return (
    <>
-   <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+   <form onSubmit={onSubmitHandler} className='pt-5 px-5 sm:pt-12 sm:pl-16'>
     <p className='text-xl'>Upload thumbnail</p>
-    <label htmlFor="">
-        
+    <label htmlFor="image">
+        <Image className='mt-4 cursor-pointer' src={image==null?'/upload-area.png':URL.createObjectURL(image)}  alt="upload" width={200} height={120} />
     </label>
+    <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="image" hidden required />
+ <p className='text-xl mt-4 '>Blog Title</p>
+ <input name='title' onChange={onChangeHandler} value={data.title} className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Type here' required />
+ <p className='text-xl mt-4 '>Blog Description</p>
+ <textarea  name='description' onChange={onChangeHandler} value={data.description} className='w-full sm:w-[500px] mt-4 px-4 py-3 border'  placeholder='Write Content here' required />
+<p className='text-xl mt-4'>Blog Category </p>
+<select  name="category"  onChange={onChangeHandler} value={data.category} className='w-40 mt-4 px-4 py-3 border text-gray-500' >
+  <option value="Startup">Startup</option>
+  <option value="Technology">Technology</option>
+  <option value="Lifestyle">Lifestyle</option>
+</select>
+<br />
+<button type='submit' className='mt-8 h-12 w-40 bg-black text-white'>ADD</button>
    </form>
    </>
   )
--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,9 @@
 import { Sidebar } from "@/Components/AdminComponents/Sidebar";
 import { Metadata } from "next";
 import Image from "next/image";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export const metadata: Metadata = {
     title: "Admin Dashboard",
     description: "Heba Hamdan Dashboard",
@@ -12,6 +15,7 @@ export default function Layout({
   }>){return(
         <>
         <div className="flex">
+          <ToastContainer theme="dark"/>
             <Sidebar/>
             <div className="flex flex-col w-full">
             <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black">
