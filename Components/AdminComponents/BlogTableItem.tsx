9b20cd79ba7f53f27dec347a68d554b2ceca8eda
HebaHamdan2 <hebasaeedhamdan296@gmail.com>
2024-10-23 21:54:54 +0300

    Blog List page for Admin

---
+++ Components/AdminComponents/BlogTableItem.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import React from 'react'
+
+const BlogTableItem = ({authorImage,title,author,date}) => {
+    const BlogDate=new Date(date);
+  return (
+   <tr className='bg-white border-b'>
+    <th scope='row' className='items-center gap-3 hidden sm:flex px-6 font-medium text-gray-900  whitespace-nowrap'>
+        <Image src={authorImage?authorImage:"/profile_icon.png"} alt="Author" height={50} width={50} />
+   <p>{author?author:"No Author"}</p>
+    </th>
+    <td className='px-6 py-4'>
+{title?title:"no title"};
+    </td>
+    <td className='px-6 py-4'>
+{BlogDate.toDateString()}
+    </td>
+    <td className='px-6 py-4 cursor-pointer'>
+x
+    </td>
+   </tr>
+  )
+}
+
+export default BlogTableItem
