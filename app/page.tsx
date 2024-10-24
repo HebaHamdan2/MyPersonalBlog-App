713b463677b9ccc55aa5d48e31e8a55af4edd1a5
HebaHamdan2 <hebasaeedhamdan296@gmail.com>
2024-10-24 13:47:55 +0300

    create EmailModel for subscription functions

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 import { BlogLsit } from "@/Components/BlogLsit";
 import { Footer } from "@/Components/Footer";
 import { Header } from "@/Components/Header";
-
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
   return (
  <>
+ <ToastContainer theme="dark"/>
 <Header/>
 <BlogLsit/>
 <Footer/>
